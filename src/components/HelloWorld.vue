10410213dccbe10635e508c7e7679492bbc45a93
Bryan Yep <yepsan95@gmail.com>
2024-11-23 13:46:05 -0500

    fix: tailwind now works on all components [1]

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,35 +9,45 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <body class="bg-gray-200 h-full w-full">
+    <h1>{{ msg }}</h1>
+    <h1 class="text-3xl font-bold underline bg-green-500 text-center">
+      Hello world! This is a deployment test
+    </h1>
+    <div class="card text-center m-5">
+      <button
+        type="button"
+        class="m-2 p-2 border-2 border-red-300 bg-red-300 hover:border-red-500 active:bg-red-500 rounded-lg"
+        @click="count++"
+      >
+        count is {{ count }}
+      </button>
+      <p class="p-2">
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+    <p class="m-5 text-center">
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+        >create-vue</a
+      >, the official Vue + Vite starter
     </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p class="m-5 text-center">
+      Learn more about IDE Support for Vue in the
+      <a
+        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+        target="_blank"
+        >Vue Docs Scaling up Guide</a
+      >.
+    </p>
+    <p class="m-5 text-[#888] text-center">Click on the Vite and Vue logos to learn more</p>
+  </body>
 </template>
 
 <style scoped>
-.read-the-docs {
+/* .read-the-docs {
   color: #888;
-}
+} */
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
